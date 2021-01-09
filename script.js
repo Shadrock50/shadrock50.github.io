aa0497faa9904d492561a909cd294f5748c1f86f
Shadrock50 <shadchristopherson@gmail.com>
2021-01-09 14:45:50 -0700

    Initial files for home page.

---
+++ script.js
@@ -0,0 +1,2 @@
+document.getElementById('lastModified').innerHTML = "<p> Last Updated: " + document.lastModified + "</p>"
+document.getElementById('current-year').innerHTML = new Date().getFullYear()
