6f2f863062b5daa26acff753b325615743bd3f0d
Shadrock50 <shadchristopherson@gmail.com>
2021-02-06 21:24:22 -0700

    Finished version

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -14,3 +14,13 @@ function createDropdown() {
     document.getElementById("footerDate").innerHTML = today.split(' ')[0] + ' ' + today.split(' ')[1] + ' ' + today.split(' ')[2] + ' ' + today.split(', ')[2];
 
 }
+
+  function displayBanner(){
+    var checkDay = ''
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+    today = new Date().toLocaleTimeString('en-us', options)
+    if (today.split(', ')[0] == "Friday") {
+        var x = document.getElementById("banner");
+        x.className = "displayedBanner";
+    }
+  }
