e13a1e0d2507e4b2e3e7c5d6ede5320b2d10e1cd
Shadrock50 <shadchristopherson@gmail.com>
2021-03-13 18:18:02 -0700

    Updated Preston Page

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,34 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=e08721ddcc9e417e3776e412ea731ea8&units=imperial';
+const fiveDayURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=e08721ddcc9e417e3776e412ea731ea8&units=imperial'
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        document.getElementById('highTemp').textContent = jsObject.main.temp_max;
+        document.getElementById('windSpeed').textContent = jsObject.wind.speed;
+        document.getElementById('humidity').textContent = jsObject.main.humidity;
+        document.getElementById('weatherCurrent').textContent = jsObject.weather[0].main
+    });
+
+fetch(fiveDayURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        let day = 1;
+        const dayofWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+
+        jsObject.list.forEach(x => {
+
+            if (x.dt_txt.includes("18:00:00")) {
+                let d = new Date(x.dt_txt);
+
+                document.getElementById(`dayOfWeek${day}`).textContent = dayofWeek[d.getDay()];
+                let imagesrc = 'https://openweathermap.org/img/w/' + x.weather[0].icon + '.png';
+                let altText = x.weather[0].description;
+                document.getElementById(`day${day}`).setAttribute('src', imagesrc);
+                document.getElementById(`day${day}`).setAttribute('alt', altText);
+                document.getElementById(`description${day}`).innerHTML = x.main.temp + "&#176;F"
+                day++;
+
+            };
+        });
+    });
---
+++ lesson10/js/wind-chill.js
@@ -0,0 +1,14 @@
+function windChill() {
+    let initTemp = document.getElementById("highTemp").innerText;
+    let windSpeed = document.getElementById("windSpeed").innerText;
+    var windChill = 0;
+
+    if (initTemp < 50 && windSpeed > 3.0) {
+        windChill = 35.74 + (0.6215 * initTemp) - (35.75 * Math.pow(windSpeed, .16)) + (0.4275 * initTemp * Math.pow(windSpeed, .13));
+        windChill = Math.trunc(windChill);
+        document.getElementById("windChill").innerHTML = windChill + " &#176;f";
+    }
+    else{
+        document.getElementById("windChill").innerHTML = "N/A";
+    }
+}
